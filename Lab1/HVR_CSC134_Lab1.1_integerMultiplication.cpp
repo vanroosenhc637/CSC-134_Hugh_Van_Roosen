3ef525b9459d775b4f9d20d57d4e0770d55b8047
Hugh Van Roosen <vanroosenhc637@mail.sandhills.edu>
2026-06-01 23:07:18 -0400

    Update 6/1/26 with Lab1

---
+++ Lab1/HVR_CSC134_Lab1.1_integerMultiplication.cpp
@@ -0,0 +1,13 @@
+/*
+Author: Hugh Van Roosen
+Date: 6/1/2026
+Purpose: This program calculates the product of two integers.
+*/
+
+//include iostream for I/O
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+}
